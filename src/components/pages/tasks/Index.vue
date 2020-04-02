ad0f442e31b045972e9cd1189e05d886ceb37b37
日野恵太 <keitahino0415@gmail.com>
2020-04-02 22:23:28 +0900

    カンバンのモック作成

--- src/components/pages/tasks/Index.vue
+++ src/components/pages/tasks/Index.vue
@@ -1,9 +1,110 @@
 <template>
   <v-content>
     <v-container>
-      <draggable tag="ul">
+
+    <!-- <draggable tag="ul">
+      <li v-for="(task) in tasks" :key="task.id">{{task.name}}</li>
+    </draggable> -->
+
+      <!-- <draggable tag="div"
+        class="mx-auto v-card v-sheet theme--light"
+        style="width: 344px;"
+        width="344"
+      >
+        <v-card-text>
+          <div>#19</div>
+          <p class="display-1 text--primary">
+            デザインの修正
+          </p>
+          <p>期限：2020/03/20</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            text
+            color="deep-purple accent-4"
+          >
+            Show Detail
+          </v-btn>
+        </v-card-actions>
         <li v-for="(task) in tasks" :key="task.id">{{task.name}}</li>
-      </draggable>
+      </draggable> -->
+      <v-row
+        class="lighten-4"
+        justify="center" align-content="start"
+      >
+        <div id="top" class='headline font-italic font-weight-light mt-3 mb-8'>カンバンボード</div>
+      </v-row>
+
+      <div class='d-flex'>
+        <!-- TODO:列も動的に増やせるように -->
+        <v-card color="grey lighten-4 mr-5 pr-5 pl-7 pb-4" min-height="800">
+          <v-layout row wrap>
+            <v-card-subtitle class="pt-2 pb-0 pl-1 font-weight-black">未着手</v-card-subtitle>
+            <!-- 1列目 -->
+            <draggable tag="div">
+              <v-card
+                v-for="(task) in tasks" :key="task.id"
+                class="mt-2"
+                width="330"
+              >
+                <v-card-text>
+                  <div>#{{task.id}}</div>
+                  <p class="subtitle-2 text--primary">
+                    {{ task.name }}
+                  </p>
+                  <p>期限：{{ task.due_date }}</p>
+                </v-card-text>
+              </v-card>
+            </draggable>
+          </v-layout>
+        </v-card>
+
+        <!-- 2列目 -->
+        <v-card color="grey lighten-4 mr-5 pr-5 pl-6 pb-4" min-height="800">
+          <v-layout row wrap>
+            <v-card-subtitle class="pt-2 pb-0 pl-1 font-weight-black">着手中</v-card-subtitle>
+            <draggable tag="div">
+              <v-card
+                v-for="(task) in tasks" :key="task.id"
+                class="mt-2"
+                width="330"
+              >
+                <v-card-text>
+                  <div>#{{task.id}}</div>
+                  <p class="subtitle-2 text--primary">
+                    {{ task.name }}
+                  </p>
+                  <p>期限：{{ task.due_date }}</p>
+                </v-card-text>
+              </v-card>
+            </draggable>
+          </v-layout>
+        </v-card>
+
+        <!-- 3列目 -->
+        <v-card color="grey lighten-4 mr-5 pr-6 pl-6 pb-4" min-height="800">
+          <v-layout row wrap>
+            <v-card-subtitle class="pt-2 pb-0 pl-1 font-weight-black">完了</v-card-subtitle>
+            <draggable tag="div">
+              <v-card
+                v-for="(task) in tasks" :key="task.id"
+                class="mt-2"
+                width="330"
+              >
+                <v-card-text>
+                  <div>#{{task.id}}</div>
+                  <p class="subtitle-2 text--primary">
+                    {{ task.name }}
+                  </p>
+                  <p>期限：{{ task.due_date }}</p>
+                </v-card-text>
+              </v-card>
+            </draggable>
+          </v-layout>
+        </v-card>
+
+      </div>
+
     </v-container>
   </v-content>
 </template>
@@ -22,28 +123,32 @@
             name: 'タスク管理用のコンポーネント追加',
             status: 'unstarted',
             start_date: '2020/03/01',
-            due_date: '2020/03/20'
+            due_date: '2020/03/20',
+            display_order: 1,
           },
           {
             id: 2,
             name: 'デザインの修正',
             status: 'unstarted',
             start_date: '2020/03/02',
-            due_date: '2020/03/21'
+            due_date: '2020/03/21',
+            display_order: 2
           },
           {
             id: 3,
             name: 'vue-router導入',
             status: 'in_progress',
             start_date: '2020/03/03',
-            due_date: '2020/03/22'
+            due_date: '2020/03/22',
+            display_order: 1
           },
           {
             id: 4,
             name: 'vuetify導入',
             status: 'done',
             start_date: '2020/03/04',
-            due_date: '2020/03/23'
+            due_date: '2020/03/23',
+            display_order: 1
           }
         ]
       }
