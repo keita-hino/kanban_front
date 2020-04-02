1b9d4e74e1eb19c55c150191dab2115de509a56a
日野恵太 <keitahino0415@gmail.com>
2020-04-02 10:23:42 +0900

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="search-ip">
+    <div>
+      <div>
+        <input @click="getIp" type="button" value="IPを取得">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SearchIp',
+
+    data() {
+      return {
+        users: {}
+      }
+    },
+
+    methods: {
+      getIp() {
+        this.axios.get('http://localhost:3000/users')
+          .then((response) => {
+            console.log(response.data);
+          })
+      }
+    }
+  }
+</script>
