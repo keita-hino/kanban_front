ca7a549c442fd883ccb9b1d56961553613f4a8d0
日野恵太 <keitahino0415@gmail.com>
2020-04-05 18:22:33 +0900

    不要な処理削除

--- src/components/TaskDetailModal.vue
+++ src/components/TaskDetailModal.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
     <v-dialog v-model="isTaskDetailModalShow" persistent max-width="600px">
-      <template v-slot:activator="{ on }">
-        <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
-      </template>
       <v-card>
         <v-card-title>
           <span class="headline">タスク設定</span>
@@ -70,11 +67,6 @@
     },
     data() {
       return {
-        dialog: false,
-        options: {
-          group: "myGroup",
-          animation: 200
-        },
         task: {}
       }
     },
@@ -85,13 +77,6 @@
         this.task.status = this.taskStatus;
         this.$emit('on-click-task-detail-save', this.task);
       },
-    },
-
-    mounted(){
-      // this.getTasks();
-    },
-
-    computed: {
     }
   }
 </script>
