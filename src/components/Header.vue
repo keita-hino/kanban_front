95247b5ad79cea1af44b94670aa26007d0501cfc
日野恵太 <keitahino0415@gmail.com>
2020-04-13 16:04:53 +0900

    不要な処理削除

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,63 +1,29 @@
  <template>
-    <!-- <v-app id="inspire"> -->
-    <!-- <div> -->
-      <v-app-bar
-        color="blue lighten-1"
-        dense
-        dark
-        app
-      >
-        <!-- TODO:下記の左側に表示されるアイコン。ただ、なぜかうまく表示されないため一旦コメントアウト -->
-        <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+  <v-app-bar
+    color="blue lighten-1"
+    dense
+    dark
+    app
+  >
 
-        <v-toolbar-title>Task管理</v-toolbar-title>
+    <v-toolbar-title>Task管理</v-toolbar-title>
 
-        <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
 
-        <span class="pr-2">{{ fullName() }}</span>
+    <span class="pr-2">{{ fullName() }}</span>
 
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-              <div v-on="on">
-                <v-btn icon>
-                  <v-icon>mdi-logout</v-icon>
-                </v-btn>
-              </div>
-              </template>
-          <span>ログアウト</span>
-        </v-tooltip>
-
-        <!-- <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
-        </v-btn>
-
-        <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-
-        <v-menu
-          left
-          bottom
-        >
-          <template v-slot:activator="{ on }">
-            <v-btn icon v-on="on">
-              <v-icon>mdi-dots-vertical</v-icon>
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+          <div v-on="on">
+            <v-btn icon>
+              <v-icon>mdi-logout</v-icon>
             </v-btn>
+          </div>
           </template>
+      <span>ログアウト</span>
+    </v-tooltip>
 
-          <v-list>
-            <v-list-item
-              v-for="n in 5"
-              :key="n"
-              @click="() => {}"
-            >
-              <v-list-item-title>Option {{ n }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu> -->
-      </v-app-bar>
-    <!-- </div> -->
-  <!-- </v-app> -->
+  </v-app-bar>
 </template>
 
 
