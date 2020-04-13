1e316d076d63663c60ea986d4aff8a57c9e369bf
日野恵太 <keitahino0415@gmail.com>
2020-04-13 16:03:54 +0900

    ヘッダー修正

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
  <template>
     <!-- <v-app id="inspire"> -->
-    <div>
+    <!-- <div> -->
       <v-app-bar
         color="blue lighten-1"
         dense
@@ -13,6 +13,19 @@
         <v-toolbar-title>Task管理</v-toolbar-title>
 
         <v-spacer></v-spacer>
+
+        <span class="pr-2">{{ fullName() }}</span>
+
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+              <div v-on="on">
+                <v-btn icon>
+                  <v-icon>mdi-logout</v-icon>
+                </v-btn>
+              </div>
+              </template>
+          <span>ログアウト</span>
+        </v-tooltip>
 
         <!-- <v-btn icon>
           <v-icon>mdi-heart</v-icon>
@@ -43,17 +56,23 @@
           </v-list>
         </v-menu> -->
       </v-app-bar>
-    </div>
+    <!-- </div> -->
   <!-- </v-app> -->
 </template>
 
 
 <script>
+  import Store from '../store'
   export default {
     data: function () {
       return {
         drawer: false
       }
     },
+    methods: {
+      fullName(){
+        return `${Store.state.auth.last_name} ${Store.state.auth.first_name}`
+      }
+    }
   }
 </script>
