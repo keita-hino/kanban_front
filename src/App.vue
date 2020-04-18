127c9e8e8c2825d84e8f6f3aec81206e0359e788
日野恵太 <keitahino0415@gmail.com>
2020-04-18 16:12:10 +0900

    サイドバーのモック作成

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,74 @@
 <template>
   <v-app id="inspire">
     <Header/>
-    <div class="container">
-      <router-view/>
-    </div>
+    <!-- <SideBar/> -->
+    <v-card app class="d-flex">
+      <v-navigation-drawer
+        app
+        v-model="drawer"
+        :mini-variant.sync="mini"
+        permanent
+      >
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-title>John Leider</v-list-item-title>
+
+          <v-btn
+            icon
+            @click.stop="mini = !mini"
+          >
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense>
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+        </v-navigation-drawer>
+      <div class="container">
+        <router-view/>
+      </div>
+    </v-card>
   </v-app>
 </template>
 
 <script>
   import Header from './components/Header'
+  // import SideBar from './components/SideBar'
   import Store from './store'
 
   export default {
     data: function () {
       return {
+        drawer: true,
+        items: [
+          { title: 'Home', icon: 'mdi-home-city' },
+          { title: 'My Account', icon: 'mdi-account' },
+          { title: 'Users', icon: 'mdi-account-group-outline' },
+        ],
+        mini: true,
       }
     },
     components: {
-      Header
+      Header,
+      // SideBar
     },
 
     mounted(){
