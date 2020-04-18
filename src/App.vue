52958af36d75acb97e1ed78d8c664ecee11ef188
日野恵太 <keitahino0415@gmail.com>
2020-04-18 19:16:46 +0900

    サイドバーのモック修正

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,59 @@
 <template>
   <v-app id="inspire">
-    <Header/>
     <!-- <SideBar/> -->
-    <v-card app class="d-flex">
+    <v-card
+      app
+      class="mx-auto"
+      height="300"
+      width="330"
+    >
       <v-navigation-drawer
         app
-        v-model="drawer"
-        :mini-variant.sync="mini"
         permanent
+        width="100%"
       >
-        <v-list-item class="px-2">
-          <v-list-item-avatar>
-            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
-          </v-list-item-avatar>
-
-          <v-list-item-title>John Leider</v-list-item-title>
-
-          <v-btn
-            icon
-            @click.stop="mini = !mini"
+        <v-row class="fill-height" no-gutters>
+          <v-navigation-drawer
+            app
+            dark
+            mini-variant
+            mini-variant-width="56"
+            permanent
           >
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
-        </v-list-item>
+            <v-list-item class="px-2">
+              <v-list-item-avatar>
+                <v-img src="https://randomuser.me/api/portraits/women/75.jpg"></v-img>
+              </v-list-item-avatar>
+            </v-list-item>
 
-        <v-divider></v-divider>
+            <v-divider></v-divider>
 
-        <v-list dense>
-          <v-list-item
-            v-for="item in items"
-            :key="item.title"
-            link
-          >
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
+            <v-list
+              dense
+              nav
+            >
 
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-        </v-navigation-drawer>
-      <div class="container">
-        <router-view/>
-      </div>
+              <v-list-item
+                v-for="item in items"
+                :key="item.title"
+                @click="''"
+              >
+                <v-list-item-action>
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-action>
+
+                <v-list-item-content>
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-navigation-drawer>
+          <Header/>
+          <div class="container">
+            <router-view/>
+          </div>
+        </v-row>
+      </v-navigation-drawer>
     </v-card>
   </v-app>
 </template>
@@ -57,11 +66,9 @@
   export default {
     data: function () {
       return {
-        drawer: true,
         items: [
-          { title: 'Home', icon: 'mdi-home-city' },
-          { title: 'My Account', icon: 'mdi-account' },
-          { title: 'Users', icon: 'mdi-account-group-outline' },
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' },
         ],
         mini: true,
       }
