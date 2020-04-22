076ce2d156f1e0a96bc32ec296dcf44cc4748852
日野恵太 <keitahino0415@gmail.com>
2020-04-22 17:35:49 +0900

    サイドバーのモック作成

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,24 @@
 <template>
   <v-app id="inspire">
-    <Header v-if="!isLogined()"/>
-    <!-- <SideBar/> -->
-    <v-card
-      v-else-if="isLogined()"
-      app
-      class="mx-auto"
-      height="300"
-      width="330"
-    >
-      <v-navigation-drawer
-        app
-        permanent
-        width="100%"
-      >
-        <v-row class="fill-height" no-gutters>
-          <v-navigation-drawer
-            app
-            dark
-            mini-variant
-            mini-variant-width="56"
-            permanent
-          >
+    <template v-if="!isLogined()">
+      <Header/>
+      <div class="container">
+        <router-view/>
+      </div>
+    </template>
 
-            <v-divider></v-divider>
-
-            <v-list
-              dense
-              nav
-            >
-
-              <v-list-item
-                v-for="item in items"
-                :key="item.title"
-                @click="''"
-              >
-                <v-list-item-action>
-                  <v-icon>{{ item.icon }}</v-icon>
-                </v-list-item-action>
-
-                <v-list-item-content>
-                  <v-list-item-title>{{ item.title }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
-          </v-navigation-drawer>
-          <Header/>
-          <div class="container">
-            <router-view/>
-          </div>
-        </v-row>
-      </v-navigation-drawer>
-    </v-card>
+    <SideBar v-else-if="isLogined()">
+      <Header/>
+      <div class="container">
+        <router-view/>
+      </div>
+    </SideBar>
   </v-app>
 </template>
 
 <script>
   import Header from './components/Header'
-  // import SideBar from './components/SideBar'
+  import SideBar from './components/SideBar'
   import Store from './store'
 
   export default {
@@ -72,7 +33,7 @@
     },
     components: {
       Header,
-      // SideBar
+      SideBar
     },
 
     methods:{
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,46 +1,50 @@
 <template>
-  <div app>
-    <v-card app class="d-flex">
+  <v-card
+    app
+    class="mx-auto"
+    height="300"
+    width="330"
+  >
     <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant.sync="mini"
+      app
       permanent
+      width="100%"
     >
-      <v-list-item class="px-2">
-        <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-title>John Leider</v-list-item-title>
-
-        <v-btn
-          icon
-          @click.stop="mini = !mini"
+      <v-row class="fill-height" no-gutters>
+        <v-navigation-drawer
+          app
+          dark
+          mini-variant
+          mini-variant-width="56"
+          permanent
         >
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
-      </v-list-item>
 
-      <v-divider></v-divider>
+          <v-divider></v-divider>
 
-      <v-list dense>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
+          <v-list
+            dense
+            nav
+          >
 
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+            <v-list-item
+              v-for="item in items"
+              :key="item.title"
+              @click="''"
+            >
+              <v-list-item-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+        <slot/>
+      </v-row>
     </v-navigation-drawer>
   </v-card>
-  </div>
 </template>
 
 <script>
