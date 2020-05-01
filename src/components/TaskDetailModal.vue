ec08f93bff24dc97b5b80eca802917e3273c0668
日野恵太 <keitahino0415@gmail.com>
2020-05-01 15:32:10 +0900

    タスク詳細設定用モーダルの更新ボタンにバリデーションエラーがあったら押せなくする制御追加

--- src/components/TaskDetailModal.vue
+++ src/components/TaskDetailModal.vue
@@ -7,69 +7,71 @@
         </v-card-title>
         <v-card-text>
           <v-container>
-            <v-row>
-              <v-col cols="12">
-                <v-text-field
-                  v-model="selectedTask.name"
-                  label="タスク名*"
-                  :counter="50"
-                  :rules="nameRules"
-                  required
-                >
-                </v-text-field>
-              </v-col>
-
-              <v-col cols="12">
-                <v-menu
-                  v-model="menu2"
-                  :close-on-content-click="false"
-                  :nudge-right="40"
-                  transition="scale-transition"
-                  offset-y
-                  min-width="290px"
-                >
-                  <template v-slot:activator="{ on }">
-                    <v-text-field
-                      v-model="selectedTask.due_date"
-                      label="期限日"
-                      readonly
-                      v-on="on"
-                    ></v-text-field>
-                  </template>
-                  <v-date-picker
-                    v-model="selectedTask.due_date"
-                    no-title
-                    @input="menu2 = false"
+            <v-form ref="task_form">
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="selectedTask.name"
+                    label="タスク名*"
+                    :counter="50"
+                    :rules="nameRules"
+                    required
                   >
-                  </v-date-picker>
-                </v-menu>
-              </v-col>
+                  </v-text-field>
+                </v-col>
 
-              <v-col cols="12">
-                <v-select v-model="selectedTask.priority"
-                  :items="priorities"
-                  label="優先度"
-                >
-                </v-select>
-              </v-col>
+                <v-col cols="12">
+                  <v-menu
+                    v-model="menu2"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        v-model="selectedTask.due_date"
+                        label="期限日"
+                        readonly
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker
+                      v-model="selectedTask.due_date"
+                      no-title
+                      @input="menu2 = false"
+                    >
+                    </v-date-picker>
+                  </v-menu>
+                </v-col>
 
-              <v-col cols="12">
-                <v-textarea
-                  v-model="selectedTask.detail"
-                  label="詳細"
-                  :counter="200"
-                  :rules="detailRules"
-                  rows="1"
-                ></v-textarea>
-              </v-col>
-            </v-row>
+                <v-col cols="12">
+                  <v-select v-model="selectedTask.priority"
+                    :items="priorities"
+                    label="優先度"
+                  >
+                  </v-select>
+                </v-col>
+
+                <v-col cols="12">
+                  <v-textarea
+                    v-model="selectedTask.detail"
+                    label="詳細"
+                    :counter="200"
+                    :rules="detailRules"
+                    rows="1"
+                  ></v-textarea>
+                </v-col>
+              </v-row>
+            </v-form>
           </v-container>
           <small class="red--text lighten-1--text">*は必須項目です</small>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="blue darken-1" text @click="$emit('on-click-task-detail-cancel')">キャンセル</v-btn>
-          <v-btn color="blue darken-1" text @click="onClickSave()">保存</v-btn>
+          <v-btn color="blue darken-1" text @click="onClickSave()" :disabled="isConserve()">保存</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -120,6 +122,11 @@
         this.task.status = this.taskStatus;
         this.$emit('on-click-task-detail-save', this.selectedTask);
       },
+
+      // バーリデーションエラーがないか
+      isConserve(){
+        return !!this.$refs.task_form && !this.$refs.task_form.validate();
+      }
     }
   }
 </script>
