6cb69d932ea4ef025e6af1c4d007d3f55884afeb
keita <keitahino0415@gmail.com>
2020-05-24 16:15:41 +0900

    ts導入

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,25 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: true, /* コンパイルのみで型検査を行わない */
+                appendTsSuffixTo: [/\.vue$/] /* .vueファイルをTSとして読み込むようにする */
+              }
+            }
+          ]
+        },
+      ]
+    }
+  }
 }
